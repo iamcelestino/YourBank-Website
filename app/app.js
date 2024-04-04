003d3ac1b2245cc67408983c9f9e1541058a6e06
iamcelestino <trcelestino488@gmail.com>
2024-04-04 04:41:22 -0700

    feat: add tabs

--- app/app.js
+++ app/app.js
@@ -17,11 +17,57 @@ burgerMenu.forEach(item => item.addEventListener('click', showMenu, true));
 
 
 
-//FREQUENTLY ASQKED QUESTIONS
-document.querySelector('.questions').addEventListener("click", event => {
+//ADD INTARECTIVTY IN FREQUENTLY ASQKED QUESTIONS
+const questionsParent = document.querySelector('.questions');
+questionsParent.addEventListener("click", event => {
     const moreQuestions = event.target.parentElement.previousElementSibling; //travesing the DOM to get previous sibling
     moreQuestions.classList.remove('active');
 });
+
+
+
+//ADDS INTERACTIVITY TO TABS
+function tabs(event, tabButtons, tabContent) {
+    const id = event.target.dataset.id;
+    if (id) {
+        tabButtons.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        event.target.classList.add('active');
+
+        tabContent.forEach(product__box => {
+            product__box.classList.remove('active');
+        });
+
+        const element = document.getElementById(id);
+        if (element) {
+            element.classList.add('active');
+        }
+    }
+}
+
+//query items in the product tab
+const product__container = document.querySelector('#products__section .container');
+const tab_buttons = document.querySelectorAll('tab_button');
+const products__box = document.querySelectorAll('.products__box');
+
+//add event in product container
+product__container.addEventListener("click", (event) => tabs(event, tab_buttons, products__box));
+
+//query items in the features tab
+const featureTabs = document.querySelector('.feature__tabs');
+const featureTabButtons = document.querySelectorAll('.feature__tab__button');
+const features = document.querySelectorAll('.features');
+
+featureTabs.addEventListener("click", event => tabs(event, featureTabButtons, features ));
+
+
+const container = document.querySelector('#testimonials__section .container');
+const testimonialTabButton = document.querySelectorAll('.testimonial__tab .tab_button');
+const testimonialsContainer = document.querySelectorAll('.testimonials__container');
+container.addEventListener("click", event => tabs(event, testimonialTabButton,testimonialsContainer));
+
+
 
 
 
